fb203eeed47cc283a377444a2e818706852b4d2c
Iqra Masroor <masrooriqra@gmail.com>
2020-02-15 19:46:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here
+let var1 = 31;
+let var2 = 2;
+
+const multiply = var1 * var2;
+const random = Math.floor(Math.random()*1) +1
+const mod = 16 % 6;
+const max = (10,12, 14, 20);
