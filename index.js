559e56da98302d547b754ecd0fa7235f10edf12b
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-09 01:08:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+const var1 = 15.5;
+let var2 = 4;
+
+let multiply = var1 * var2
+
+const random = Math.floor(Math.random() * 100) + 1;
+const mod = 29 % 5;
+
+const max = Math.max(1,8,20,16,5);
